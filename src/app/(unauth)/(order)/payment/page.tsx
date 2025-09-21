94da71392075436857f2a7fb69c9c93493a6c00c
NToan324 <nhattoan664t@gmail.com>
2025-09-22 00:47:13 +0700

    fix: remove unused Button import in payment page

--- src/app/(unauth)/(order)/payment/page.tsx
+++ src/app/(unauth)/(order)/payment/page.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { IoIosArrowDown } from 'react-icons/io'
 
-import { Button } from '@/components/ui/button'
 import { formatPrice } from '@/utils/helpers'
 
 export default function page() {
