7d25f8183e9fc49a1db8cd0073b88fdd0f2122e9
NToan324 <nhattoan664t@gmail.com>
2025-12-02 21:35:51 +0700

    format

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,5 @@
 import { httpClient as axios } from '@/http/index'
-import { ApiResponse, Pagination } from '@/http/types/http.response'
+import { ApiResponse } from '@/http/types/http.response'
 import { Profile, UpdateProfile, UserPagination } from '@/types/user.type'
 
 class UserService {
@@ -7,7 +7,7 @@ class UserService {
     const response = await axios.get<ApiResponse<Profile>>('/user/profile')
     return response.data
   }
-  
+
   updateProfile = async (payload: Partial<UpdateProfile>) => {
     const response = await axios.put<ApiResponse<Profile>>('/user/profile', payload)
     return response.data
